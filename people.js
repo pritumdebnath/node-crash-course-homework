3173889f87dba5762cd640aa3180c5c4a79c4a9e
pritumdebnath <122291993+pritumdebnath@users.noreply.github.com>
2023-02-27 16:42:01 -0500

    new push

---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['Arsenal', 'Manchester', 'Totenham', 'Liverpool']
+const ages = [20, 25, 30, 35];
+
+// console.log(people);
+
+module.exports = {
+    people: people,
+    ages: ages
+};
---
+++ test.js
@@ -0,0 +1,10 @@
+// const name = "Donkey Kong";
+
+// console.log(name);
+
+const greet = (name) => {
+    console.log(`hello, ${name}`);
+}
+
+greet('Nes');
+greet('Pikachu');
